10cb90b25be83cd78573eeef6f3facaea6be6d78
Prince2613-ank <Prince2613-ank@users.noreply.github.com>
2026-05-28 13:15:55 +0530

    Use updated third floor model

--- cesium_demo/src/models.ts
+++ cesium_demo/src/models.ts
@@ -32,7 +32,7 @@ import secondFloorUrl from "../final_2nd_floor_without_chair_fast.glb?url";
 import fullBuildingUrl from "../full_building.glb?url";
 import groundFloorUrl from "../ground_floor_final.glb?url";
 import outdoorModelUrl from "../outdoor_model.glb?url";
-import thirdFloorUrl from "../3rd_floor_without_chairs_fast.glb?url";
+import thirdFloorUrl from "../3rd_floor_without_chairs.glb?url";
 import secondFloorLoadingBaseUrl from "../2nd_floor_base.glb?url";
 import secondFloorLoadingBaseWallUrl from "../2nd_floor_base_wall.glb?url";
 import thirdFloorLoadingBaseUrl from "../3rd_floor_base.glb?url";
